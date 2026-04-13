e329b33ce423cf353c4814ea772f07c7a1aaa46d
raginee4321 <sraginee412@gmail.com>
2026-04-13 14:28:51 +0530

    Force secure Cloudinary URLs

--- server/index.js
+++ server/index.js
@@ -26,7 +26,8 @@ mongoose.connect(MONGODB_URI)
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true
 });
 
 // Cloudinary Storage for Multer
